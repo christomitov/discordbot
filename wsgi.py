146837928f45e59e4aec24f3cb8ff981b4450689
Christo Mitov <christo.mitov@gmail.com>
2024-09-02 13:21:23 -0400

    add wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,28 @@
+import sys
+import logging
 from dashboard.dashboard import app
 from bot.bot import run_bot
 import threading
 
+logging.basicConfig(level=logging.DEBUG, filename='/home/botuser/discordbot/app.log', filemode='a',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 def run_flask():
     app.run()
 
+def run_app():
+    try:
+        bot_thread = threading.Thread(target=run_bot)
+        flask_thread = threading.Thread(target=run_flask)
+        bot_thread.start()
+        flask_thread.start()
+        bot_thread.join()
+        flask_thread.join()
+    except Exception as e:
+        logging.error(f"Error in run_app: {str(e)}", exc_info=True)
+        sys.exit(1)
+
+application = app
+
 if __name__ == "__main__":
-    bot_thread = threading.Thread(target=run_bot)
-    flask_thread = threading.Thread(target=run_flask)
-
-    bot_thread.start()
-    flask_thread.start()
-
-    bot_thread.join()
-    flask_thread.join()
+    run_app()
